e4094151c97a511f83b0ba46f45b3e0c6b985b3a
Brian Grubba <brian@snarebox.com>
2025-08-04 11:16:51 -0400

    chore: set up zod schema generation

---
+++ packages/types/ts-to-zod.config.cjs
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'common',
+    input: 'src/common.ts',
+    output: 'src/zod/common.zod.ts'
+  },
+  {
+    name: 'memory',
+    input: 'src/memory.ts',
+    output: 'src/zod/memory.zod.ts'
+  },
+  {
+    name: 'chat',
+    input: 'src/chat.ts',
+    output: 'src/zod/chat.zod.ts'
+  },
+  {
+    name: 'mca',
+    input: 'src/mca.ts',
+    output: 'src/zod/mca.zod.ts'
+  }
+];
